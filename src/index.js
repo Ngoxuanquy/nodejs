9b1e4272a4ad4cf4ed9ed88cf7193f9a8d81e0e0
Quy Ngô Xuân <ngoxuanquy1812@gmail.com>
2022-08-22 10:06:36 +0700

    nodejs

--- src/index.js
+++ src/index.js
@@ -31,6 +31,6 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
-
-app.listen(3000);
+const PORT = process.env.port || 3000;
+app.listen(PORT);
 console.log("http://localhost:3000/")
